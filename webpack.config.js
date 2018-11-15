7e3c4e1255ce48c0ffc93d162e6949036a08d6f8
liukaiyang <liukaiyang@souche.com>
2018-11-15 12:04:56 +0800

    setting update

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,20 @@
-const path = require('path'),
-  webpack = require('webpack'),
-  CleanWebpackPlugin = require('clean-webpack-plugin'),
-  HtmlWebpackPlugin = require('html-webpack-plugin');
-
-// const extractPlugin = new ExtractTextPlugin({ filename: './assets/css/app.css' });
+const path = require('path');
+const merge = require('webpack-merge');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
-
   // absolute path for project root
   context: path.resolve(__dirname, 'src'),
-
   entry: {
     // relative path declaration
     app: './app.js',
   },
-
   output: {
     // absolute path declaration
     path: path.resolve(__dirname, 'dist'),
     filename: './assets/js/[name].[chunkhash].js'
   },
-
   module: {
     rules: [
       // babel-loader with 'env' preset
@@ -56,17 +50,15 @@ const config = {
           }
         ]
       },
-      // file-loader(for images)
-      { test: /\.(jpg|png|gif|svg)$/, use: [{ loader: 'url-loader', options: {limit:5120, name: '[name].[ext]', outputPath: './assets/media/' } }] },
+      // url-loader(for images)
+      { test: /\.(jpg|png|gif|svg)$/, use: [{ loader: 'url-loader', options: { limit: 5120, name: '[name].[ext]', outputPath: './assets/media/' } }] },
       // file-loader(for fonts)
       { test: /\.(woff|woff2|eot|ttf|otf)$/, use: ['file-loader'] }
-
     ]
   },
 
   plugins: [
     // cleaning up only 'dist' folder
-
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       minify: false,
@@ -74,15 +66,39 @@ const config = {
     }),
   ],
 
-  devServer: {
-    // static files served from here
-    contentBase: path.resolve(__dirname, "./dist/assets/media"),
-    compress: true,
-    // open app in localhost:2000
-    port: 2000,
-    stats: 'errors-only',
-    open: true
-  },
-  devtool: 'inline-source-map'
 };
-module.exports = config;
+
+module.exports = (env, argv) => {
+  console.log(env);
+  if (argv.mode === 'development') {
+    return merge(config, {
+      devtool: 'inline-source-map',
+      devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        open: true,
+        host: 'localhost', // 0.0.0.0 localhost
+        port: 8088,
+      }
+    });
+  } else if (argv.mode === 'production') {
+    return merge(config, {
+      optimization: {
+        splitChunks: {
+          cacheGroups: {
+            commons: {
+              name: "commons",
+              chunks: "initial",
+              minChunks: 2
+            }
+          }
+        }
+      },
+      plugins: [
+        new UglifyJSPlugin()
+      ]
+    });
+  } else {
+    throw new Error("请指定开发环境 --mode development or production");
+  }
+
+};
